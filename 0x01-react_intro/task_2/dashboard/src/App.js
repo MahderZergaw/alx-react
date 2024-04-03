645138dc69fb12920ad52c01d435184193394fcb
MahderZergaw <mahderzergaw640@gmail.com>
2024-04-03 21:17:07 +0300

    add email and password

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./HolbertonLogo.jpg";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlfor='email'>email</label>
+        <input name='email' type='email'></input>
+        <label htmlfor='password'>password</label>
+        <input name='password' type ='password'></input>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
