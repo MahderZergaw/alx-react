b9016364eb07edcc5d394913e2913a8c38e0f323
MahderZergaw <mahderzergaw640@gmail.com>
2024-04-03 21:49:00 +0300

    'all'

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Notifications from './Notifications'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-const root_notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
-root_notifications.render(
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
   <React.StrictMode>
-    <Notifications/>
+    <Notifications />
   </React.StrictMode>
 );
+
 reportWebVitals();
